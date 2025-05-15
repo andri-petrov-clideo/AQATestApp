3fc7d5c6a81ed9379f77e3316c8a1b0b622b9220
Andrii Petrov <andrii.petrov@clideo.com>
2025-05-15 12:55:02 +0100

    Initial Commit

---
+++ AQATestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AQATestApp
+//
+//  Created by Andrii Petrov on 15/05/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
